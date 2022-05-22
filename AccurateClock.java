1462a170e4d50a3b386cd8bbd42e89c5b18a75c1
Liat Tsipory <liat.tsipory@campus.technion.ac.il>
2022-05-22 12:15:19 +0300

    clock and accurate clock

---
+++ AccurateClock.java
@@ -0,0 +1,12 @@
+public class AccurateClock extends Clock {
+    private int seconds;
+
+    public AccurateClock(int hour, int minute, int seconds) {
+        super(hour, minute);
+        this.seconds = seconds;
+    }
+
+    /*
+    Additional methods...
+     */
+}
---
+++ Clock.java
@@ -0,0 +1,14 @@
+public class Clock {
+    private int hour;
+    private int minute;
+
+    public Clock(int hour, int minute) {
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    /*
+    Additional methods...
+     */
+}
+
