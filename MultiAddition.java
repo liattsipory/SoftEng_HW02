866c6782a09c0f0cbc6c526587fdfde6a8570501
Liat Tsipory <liat.tsipory@campus.technion.ac.il>
2022-05-23 15:43:45 +0300

    hi

--- MultiAddition.java
+++ MultiAddition.java
@@ -1,3 +1,35 @@
-public class MultiAddition extends Addition {
+public class MultiAddition extends Expression {
+    private double value;
+    private String printedValue;
+
+    public MultiAddition(Expression... values) {
+        int sum = 0;
+        for (Expression value : values) {
+            sum += value.evaluate();
+        }
+        this.value = sum;
+        for (Expression value : values) {
+            String string_val = value.toString();
+            this.printedValue = String.join(")+(", string_val);
+        }
+
+        }
+        @Override
+        public double evaluate () {
+            return this.value;
+        }
+
+        @Override
+        public String toString () {
+            return this.printedValue;
+        }
+    }
+//    public MultiAddition() {
+//        this.value = 0;
+//        this.printedValue = "";
+
+
+
+
 
 }
