7dd42ea7cced9ffee566712b038b5f0b558ee7fb
danam <mdana@campus.technion.ac.il>
2022-05-24 20:08:31 +0300

    working!!!!!!!!!!!!

--- Multiplication.java
+++ Multiplication.java
@@ -1,4 +1,11 @@
 public class Multiplication extends MultiMultiplication {
+    /**
+     * This class represents an arithmetical expression that
+     * results after multiplying two expressions.
+     * Subclass of MultiMultiplication.
+     * @param val1 - first value of multiplication (right side of expression)
+     * @param val2 - second value of multiplication (left side of expression)
+     */
 
     public Multiplication(Expression val1, Expression val2) {
         super(val1, val2);
