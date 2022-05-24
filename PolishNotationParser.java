b74f11818414069d130182d33494cd3b028d5b63
Liat Tsipory <liat.tsipory@campus.technion.ac.il>
2022-05-24 11:05:09 +0300

    I know its not really good right know i have to leave but we will continue

--- PolishNotationParser.java
+++ PolishNotationParser.java
@@ -5,25 +5,73 @@ static final int DOT = '.';
     @Override
     public Expression parse(String string) {
         Expression finalExpression;
-        String[] input = string.split("[ ]", 0);
+        String[] input = string.split("[ ]", 0); // dont we need ( ) ?
         int len = input.length;
         if (len == 1) finalExpression = singleStringToExp(input[0]);
         else {
-            int operators = countOperators(input, len);
-            for (int i = 1; i <= operators; i++) {
+//            String[] tmp = new String[len];
+            int operators_num = countOperators(input, len);
+            for (int i = 1; i <= operators_num; i++) {
+                calc_app(input, len);
+
 
             }
         }
         return finalExpression;
     }
 
-    private static int countOperators(String[] input, int len) {
-        int result = 0;
-        for(int i = 0; i < len; i++){
-           if (isOperator(input[i])) result += 1;
+    private static Double calc_app(String[] input, int len) { //I know its not really good right know i have to leave
+        Double result = 0.0;
+        for (int j= len-1; j>1; j--) {
+            if (input[j]=="-u" && !isOperator(input[j+1])) {
+                Expression val = singleStringToExp(input[j+1]);
+                Expression expression = new UnaryMinus(val);
+                result = expression.evaluate();
+            }
+            if (!isOperator(input[j]) && !isOperator(input[j+1]) && isOperator(input[j+2])) {
+                Expression val1 = singleStringToExp(input[j]);
+                Expression val2 = singleStringToExp(input[j+1]);
+                String curr_operator = input[j+2];
+                calculator(val1, val2, curr_operator);
+            } else continue;
+        }
+    }
+
+    private static Double calculator(Expression val1, Expression val2, String operator){
+        Expression curr_expression;
+        Double result = 0.0;
+        if (operator == "*"){
+            curr_expression = new Multiplication(val1,val2);
+            result = curr_expression.evaluate();
+        }
+        if (operator == "+"){
+            curr_expression = new Addition(val1,val2);
+            result = curr_expression.evaluate();
+        }
+        if (operator == "-"){
+            curr_expression = new Subtraction(val1,val2);
+            result = curr_expression.evaluate();
+        }
+        if (operator == "/"){
+            curr_expression = new Division(val1,val2);
+            result = curr_expression.evaluate();
         }
         return result;
     }
+
+//    private String[] create_operators_arr(String[] operators_arr, int operators_num, String[] input) {
+//        for (int i = 0; i < operators_num; i++) {
+//            operators_arr[i] =
+//        }
+
+        private static int countOperators (String[]input,int len){
+            int result = 0;
+            for (int i = 0; i < len; i++) {
+                if (isOperator(input_str[i])) result += 1;
+            }
+            return result;
+        }
+
 
     private static boolean isOperator(String string) {
         if (string == null) return false;
